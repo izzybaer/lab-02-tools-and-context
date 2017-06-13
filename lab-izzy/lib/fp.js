b47bbd790037f3ff0add6e64bba913b57aebf60c
Izabella Baer <izabellabaer@gmail.com>
2017-06-13 15:26:59 -0700

    added all stand alone functions and two tests per function

--- lab-izzy/lib/fp.js
+++ lab-izzy/lib/fp.js
@@ -2,14 +2,14 @@
 
 const fp = module.exports = {};
 
-fp.map = (list, callback) => Array.prototype.map.call(list, callback)
+fp.map = (list, callback) => Array.prototype.map.call(list, callback);
 
-// filter
-//
-// reduce
-//
-// concat
-//
-// splice
+fp.filter = (list, callback) => Array.prototype.filter.call(list, callback);
+
+fp.reduce = (acc, curr) => Array.prototype.reduce.apply(acc, curr);
+
+fp.concat = (arr, arr2) => Array.prototype.concat.apply(arr, arr2);
+
+fp.splice = (list, args) => Array.prototype.splice.apply(list, args);
 
 // using call and apply, create stand alone functions
--- lab-izzy/test/fp-test.js
+++ lab-izzy/test/fp-test.js
@@ -6,9 +6,65 @@ const fp = require('../lib/fp.js');
 describe('testing stand alones', () => {
 
   describe('testing map', () => {
-    it('it should return 2, 4, 6', () => {
+    it('it should return [2, 4, 6]', () => {
       let result = fp.map([1,2,3], n => n * 2);
-      expect(result).toEqual(2, 4, 6);
+      expect(result).toEqual([2,4,6]);
+    });
+  });
+  describe('testing map again', () => {
+    it('it should return [4, 8, 12]', () => {
+      let result = fp.map([2,4,6], n => n * 2);
+      expect(result).toEqual([4,8,12]);
+    });
+  });
+  describe('testing filter', () => {
+    it('it should return 5', () => {
+      let result = fp.filter([5,6,7], n => n < 6);
+      expect(result).toEqual([5]);
+    });
+  });
+  describe('testing filter again', () => {
+    it('it should return 6', () => {
+      let result = fp.filter([4,5,6], n => n > 5);
+      expect(result).toEqual([6]);
+    });
+  });
+  describe('testing reduce', () => {
+    it('it should return 10', () => {
+      let result = fp.reduce([2,3,5], [(a, c) => a + c]);
+      expect(result).toEqual(10);
+    });
+  });
+  describe('testing reduce again', () => {
+    it('it should return 16', () => {
+      let result = fp.reduce([3,7,6], [(a, c) => a + c]);
+      expect(result).toEqual(16);
+    });
+  });
+  describe('testing concat', () => {
+    it('it should return [0,1,2,3,4,5]', () => {
+      let result = fp.concat([0,1,2],[3,4,5]);
+      expect(result).toEqual([0,1,2,3,4,5]);
+    });
+  });
+  describe('testing concat again', () => {
+    it('it should return [5,6,7,8,9,10]', () => {
+      let result = fp.concat([5,6,7],[8,9,10]);
+      expect(result).toEqual([5,6,7,8,9,10]);
+    });
+  });
+  describe('testing splice', () => {
+    it('it should return [4, 5, 6, 7]', () => {
+      let arr = [4, 5, 6];
+      fp.splice(arr, [3, 0, 7]);
+      expect(arr).toEqual([4, 5, 6, 7]);
+    });
+  });
+  describe('testing splice again', () => {
+    it('it should return [0, 9, 2, 5, 8, 9]', () => {
+      let arr = [0, 9, 2, 5, 8];
+      fp.splice(arr, [5, 0, 9]);
+      expect(arr).toEqual([0, 9, 2, 5, 8, 9]);
     });
   });
 });
